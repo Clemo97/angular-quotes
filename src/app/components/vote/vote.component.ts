0f137584449e3d472072fa2e35797bb8c6739989
Clemo97 <lumumbaclement@gmail.com>
2022-04-11 15:09:17 +0300

    add logic for voting component

--- src/app/components/vote/vote.component.ts
+++ src/app/components/vote/vote.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+// import { Quote } from '../../quote';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-vote',
   templateUrl: './vote.component.html',
-  styleUrls: ['./vote.component.css']
+  styleUrls: ['./vote.component.css'],
 })
 export class VoteComponent implements OnInit {
+  @Input() quote;
+  @Output() thisEvent = new EventEmitter<string>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  upvote() {
+    this.quote.upvote += 1;
+    this.thisEvent.emit('upvote');
+  }
+  downvote() {
+    this.quote.downvote += 1;
+    this.thisEvent.emit('downvote');
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
