e6ce39b8219c82effde69eba314cd6f2cb6e3599
Clemo97 <lumumbaclement@gmail.com>
2022-04-11 15:02:29 +0300

    add quote-form logic

--- src/app/components/quote-form/quote-form.component.ts
+++ src/app/components/quote-form/quote-form.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Quote } from '../../quote';
 
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
-  styleUrls: ['./quote-form.component.css']
+  styleUrls: ['./quote-form.component.css'],
 })
 export class QuoteFormComponent implements OnInit {
+  public placeholder = 'Your Name...';
 
-  constructor() { }
+  public newQuote = new Quote(0, '', 0, 0, '', '', new Date());
+  @Output() addNewQuote = new EventEmitter<Quote>();
 
-  ngOnInit(): void {
+  submitQuote() {
+    let tempQuote = Object.assign({}, this.newQuote);
+    this.addNewQuote.emit(tempQuote);
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
+
