2e99957613cae9d5f722b859fca050c9deb0ca11
Clemo97 <lumumbaclement@gmail.com>
2022-04-11 14:31:54 +0300

    create quote component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header/header.component';
+import { QuoteComponent } from './components/quote/quote/quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule
